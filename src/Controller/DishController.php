66ca589bb3473561c98b83ad5db12808863ea585
PravinRaj Selvaraj <pravinraj.selvaraj@kripya.com>
2023-01-06 00:14:02 +0530

    first commit

---
+++ src/Controller/DishController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Dishes;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+    /**
+     * @Route("/dish", name="app_dish.")
+     */
+class DishController extends AbstractController
+{
+    /**
+     * @Route("/", name="dish")
+     */
+    public function index(): Response
+    {
+        return $this->render('dish/index.html.twig', [
+            'controller_name' => 'DishController',
+        ]);
+    }
+
+     /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request){
+        $dishes = new Dishes();
+        $dishes->setName('Pizza');
+
+        //Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($dishes);
+        $em->flush();
+
+        //Response
+        return new Response('Dish has been created');
+    }
+}
---
+++ src/Entity/Dishes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DishesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DishesRepository::class)
+ */
+class Dishes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $discription;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDiscription(): ?string
+    {
+        return $this->discription;
+    }
+
+    public function setDiscription(?string $discription): self
+    {
+        $this->discription = $discription;
+
+        return $this;
+    }
+}
